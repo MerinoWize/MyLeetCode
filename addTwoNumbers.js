72d9192d82e3b5cdffaa8639750b9ffbe251d7d2
Eduardo Merino <eduardo.merino@wizeline.com>
2022-07-14 12:43:14 -0500

    fix: minor errors

--- addTwoNumbers.js
+++ addTwoNumbers.js
@@ -15,17 +15,20 @@ var addTwoNumbers = function(l1, l2) {
     let currentNode2 = l2;
     let currentCarry = 0;
 
-    let result;
+    let result = new ListNode();
     let currentResult = result;
 
-    while (currentNode1.next || currentNode2.next) {
-        console.log('currentResult', currentResult);
-        currentResult = currentResult?.next ? currentResult.next : currentResult;
+    while (currentNode1 || currentNode2) {
         const rawSum = (currentNode1?.val || 0) + (currentNode2?.val || 0) + currentCarry;
         currentCarry = Math.trunc(rawSum / 10);
         const sumOnes = rawSum - (currentCarry * 10);
 
-        currentResult = listNode(sumOnes);
+        currentResult.val = sumOnes;
+
+        if (currentNode1.next || currentNode2.next) {
+            currentResult.next = new ListNode();
+            currentResult = currentResult.next;
+        }
 
         currentNode1 = currentNode1.next;
         currentNode2 = currentNode2.next;
