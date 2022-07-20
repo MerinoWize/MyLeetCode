eb73fd338d66fb42ff737a5f6f90b29b6df1f57b
Eduardo Merino <eduardo.merino@wizeline.com>
2022-07-20 17:13:34 -0500

    feat: add Number of Matching Subsequences

---
+++ numberOfMatchingSubsequences.js
@@ -0,0 +1,46 @@
+/**
+ * @param {string} s
+ * @param {string[]} words
+ * @return {number}
+ */
+ var numMatchingSubseq = function(s, words) {
+    let matchingSubs = [];
+
+    for (let i = 0; i < words.length; i++) {
+        const word =  words[i];
+
+        let j = 0;
+        let isValidSub = true;
+        let currentLatestIndex = -1;
+
+        while (j < word.length && isValidSub) {
+            const char = word[j];
+
+            let indexOfChar = s.indexOf(char, (currentLatestIndex + 1));
+            if (indexOfChar !== -1) {
+                currentLatestIndex = indexOfChar;
+            } else {
+                isValidSub = false;
+            }
+
+
+
+            j++;
+        }
+        
+        j = 0;
+        currentLatestIndex = -1;
+
+        if (isValidSub) {
+            matchingSubs.push(word);
+        }
+
+        isValidSub = true;
+    }
+
+    console.log('matchingSubs', matchingSubs);
+
+    return matchingSubs.length;
+};
+
+console.log(numMatchingSubseq("dsahjpjauf", ["ahjpjau","ja","ahbwzgqnuk","tnmlanowax"]));
