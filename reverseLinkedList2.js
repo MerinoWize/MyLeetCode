49263cfc842b3caa299cc74ad1527e071ac34ed5
Eduardo Merino <eduardo.merino@wizeline.com>
2022-07-22 17:48:37 -0500

    feat: create key pointers

--- reverseLinkedList2.js
+++ reverseLinkedList2.js
@@ -12,72 +12,64 @@
  * @return {ListNode}
  */
  var reverseBetween = function(head, left, right) {
-    const getNfromStartNode = (head, n) => {
-        let node = head;
-        let prev = null;
-
-        for (let i = 1; i <= n; i++) {
-            if (!node) {
-                break
-            }
-
-            if (i > 1) {
-                prev = node;
-                node = node.next;
-            }
-        }
-
-        return {
-            result: node, 
-            prev
-        };
-    };
-
-    const getEndOflist = (head) => {
-        let node = head;
-
-        while (node.next) {
-            node = node.next;
-        }
-
-        return node;
-    }
-
     const reverseList = (head) => {
-        let prev = null;
-        let curr = head;
-        let next = null;
-
-        while (curr) {
-            next = curr.next;
-            curr.next = prev;
-            prev = curr;
-            curr = next;
-        }
-        return prev;
-    };
-
-    const {  result: sublistEnd } = getNfromStartNode(head, right);
-    const sublisEndNext = sublistEnd.next;
-
-    sublistEnd.next = null;
-
-    const {  result: sublistStart, prev } = getNfromStartNode(head, left);
-
-    const reversedSublist = reverseList(sublistStart);
-
-    let newHead;
-
-    if (prev) {
-        prev.next = reversedSublist;
-        newHead = head;
-    } else {
-        newHead = reversedSublist;
-    }
-
-    const endOfNewList = getEndOflist(newHead);
-
-    endOfNewList.next = sublisEndNext;
-
-    return newHead;
-};
+         let prev = null;
+         let curr = head;
+         let next = null;
+ 
+         while (curr) {
+             next = curr.next;
+             curr.next = prev;
+             prev = curr;
+             curr = next;
+         }
+         return prev;
+     };
+ 
+     let startOfSublist,
+         endOfSublist,
+         beforeStartOfSublist,
+         afterEndOfSublist = null;
+ 
+     let count = 1;
+     
+     let node = head;
+     
+     while (node) {
+         if (count === left - 1) {
+             beforeStartOfSublist = node;
+         }
+ 
+         if (count === left) {
+             startOfSublist = node;
+         }
+ 
+         if (count === right) {
+             endOfSublist = node;
+         }
+ 
+         if (count === right + 1) {
+             afterEndOfSublist = node;
+         }
+ 
+         node = node.next;
+         count ++;
+     }
+ 
+     endOfSublist.next = null;
+ 
+     const reversedSublist = reverseList(startOfSublist);
+ 
+     let newHead;
+ 
+     if (beforeStartOfSublist) {
+         beforeStartOfSublist.next = reversedSublist;
+         newHead = head;
+     } else {
+         newHead = reversedSublist;
+     }
+ 
+     startOfSublist.next = afterEndOfSublist;
+ 
+     return newHead;
+ };
