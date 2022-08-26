f18daa52ff4a3044e077119f77743b1e2777d8ef
Eduardo Merino <eduardo.merino@wizeline.com>
2022-08-26 10:24:28 -0500

    longestPalindrome

---
+++ longestPalindrome.js
@@ -0,0 +1,36 @@
+/**
+ * @param {string} s
+ * @return {string}
+ */
+ var longestPalindrome = function(s) {
+    const expandAroundCenter = (s, left, right) => {
+        let L = left;
+        let R = right;
+
+        while (L >= 0 && R < s.length && s[L] == s[R]) {
+            L --;
+            R ++;
+        }
+        return R - L - 1;
+    };
+
+    if (s.length === 0) return "";
+
+    let start = 0;
+    let end = 0; // 0.5
+
+    for (let i = 0; i < s.length; i++) {
+        const len1 = expandAroundCenter(s, i, i); // 1
+        const len2 = expandAroundCenter(s, i, i + 1); // 0
+        const len = Math.max(len1, len2); // 1
+        if (len > (end - start)) {
+            start = Math.round(i - (len - 1) / 2);
+            end = Math.round(i + len / 2);
+        }
+    }
+
+    return s.substring(start, end + 1);
+
+};
+
+console.log(longestPalindrome('cbbd'));
